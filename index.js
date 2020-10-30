0c2eafe85eb18897a0fbf5943d82d26f36e3d436
Meghraj Deshmukh <meghraj.deshmukh103@gmail.com>
2020-10-31 01:37:31 +0530

    event listener added

---
+++ index.js
@@ -0,0 +1,17 @@
+
+fun();
+
+function fun()
+{
+    console.log("hey hi , its running ohk");
+}
+
+const snake=document.getElementById('mydiv');
+
+snake.addEventListener('click', clickMe);
+
+function clickMe()
+{
+    console.log("click Me");
+}
+
